cb0a0f6623eb48ae043c5e7ab81605a6e4498116
TOwInOK 60252419+TOwInOK@users.noreply.github.com <60252419+TOwInOK@users.noreply.github.com>
2025-01-09 12:18:50 +0300

    add doc strings for base aspects

--- src/aspects.rs
+++ src/aspects.rs
@@ -16,15 +16,32 @@ pub use spaceing::*;
 pub use svg::*;
 
 /// Need to be presented?
+///
+/// helpfull arugments in place of Option type.
+///
+/// **note**: mb will be deleted in future
 pub type Show = bool;
+
 /// Path to any destination
 pub type Path = String;
+
 /// Size
+///
+/// By default means only px, but it can be different for some specific layout
 pub type Size = u64;
 
-// Link
+/// Link
+///
+/// *note*: Only HTML/Leptos Attribute and it will be ignored for another render type
+///
+/// HTML Href
 pub type Link = Option<Path>;
 
 // Sizes
+/// Width
+/// presumably in pixels
 pub type Width = Size;
+
+/// Height
+/// presumably in pixels
 pub type Height = Size;
